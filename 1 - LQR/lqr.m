300836a94365044f8c3f2c1e885f77110ce5db85
lucad93 <lucad93@outlook.it>
2018-03-23 15:03:27 +0100

    Simulazione con K
    Simulazione con K invece di K_infinito

--- 1 - LQR/lqr.m
+++ 1 - LQR/lqr.m
@@ -26,13 +26,27 @@ x0 = [10 5 -3]';                    %stato inziziale (a caso)
 %Equazione di Riccati per determinare K
 [P, K, K_infinito] = riccati_P_K(A,B,Q,Qf,R,T);
 
+%Variabili per i grafici
+f_k_infinito = figure;
+f_k = figure;
+
 %provo prima con K_infinito
 sys = ss(A+B*K_infinito,B,C,D,sampleTime);
 U = zeros(5,length(T));
 [Y,Tsim,X] = lsim(sys,U,T,x0);
+figure(f_k_infinito);
 subplot(3,1,1); plot(Tsim,X(:,1));  title('x1');
 subplot(3,1,2); plot(Tsim,X(:,2));  title('x2');
 subplot(3,1,3); plot(Tsim,X(:,3));  title('x3');
+
+%provo ora con K(:,:,1)
+sys = ss(A+B*K(:,:,1),B,C,D,sampleTime);
+U = zeros(5, length(T));
+[Y,Tsim,X] = lsim(sys,U,T,x0);
+figure(f_k);
+subplot(3,1,1); plot(Tsim,X(:,1));  title('x1 (utilizzando K)');
+subplot(3,1,2); plot(Tsim,X(:,2));  title('x2 (utilizzando K)');
+subplot(3,1,3); plot(Tsim,X(:,3));  title('x3 (utilizzando K)');
 
 %Guardando i grafici si nota che la condizione x1(t)~2*x2(t)+x3(t) è
 %verificata a regime.
