d1ec7a0dd6ee815822056d836646a5aff660a880
Alessio Bollea <alessio.bollea@live.it>
2018-03-26 17:34:23 +0200

    Aggiustato script
    Le matrici R e Q andavano in conflitto con le covarianze R e Q (a cui ho
    cambiato nome). Corretta la dimensione sulla covarianza dell'errore su
    y. Rimessi grafici per vedere la correttezza di Kalman.

--- 2 - LQG/lqg.m
+++ 2 - LQG/lqg.m
@@ -1,16 +1,18 @@
 clear
 clc
 
+%Matrici dei costi per lo stato (Q) e per i controlli (R)
+Q = [2 -2 -2; -2 5 2; -2 2 2];
+Qf = Q;
+R = [17 0 -4 0 0; 0 1 0 0 0; -4 0 2 0 0; 0 0 0 2 -2; 0 0 0 -2 5];
+
 % Matrice dello stato
-%A = [-0.5 0 0; 0 -0.5 0; 0 2 -0.5];
 A = eye(3);
 
 % Matrice degli ingressi
-%B = zeros(3,5);
 B = [4 0 -1 0 0; 0 1 0 -1 0; 0 0 1 1 -1];
 
 % Matrice delle uscite
-%C = eye(3);
 C = [1 1 -1];
 D = 0;
 
@@ -19,26 +21,27 @@ u = zeros(5,1);
 abs(eig(A))
 
 %Suppongo che lo stato iniziale sia distribuito come una gaussiana con
-%valor medio [5 5 5] e covarianza [1 1 1; 1 1 1; 1 1 1]
-x(:,1) = [5 5 5]';      extX(:,1) = [5 5 5]';
-covX = ones(3);
+%valor medio [10 5 -3] e covarianza [1 0 0; 0 1 0; 0 0 1]
+x0 = [10 5 -3]';
+x(:,1) = x0;      extX(:,1) = x0;
+covX = eye(3);
 
 %Suppongo che i rumori sullo stato e sull'uscita siano a media nulla e
 %varianza unitaria
-Q = ones(3);    R = eye(3);
+covErrX = eye(3);    covErrY = 1;
 
 %Simulazione per 8 istanti:
-% T = 0:7;
 sampleTime = 1;
 horizon = 7;
 T = 0:sampleTime:horizon;
 
-
 for t=1:length(T)-1
-    x(:,t+1) = A*x(:,t) + normrnd(0,1,[3,1]);                   %vero stato
-    y = C*x(:,t+1) + normrnd(0,1,[3,1]);                        %vera uscita
-    [extX(:,t+1), covX] = ourKalman(A,B,C,x(:,t),u,y,covX,Q,R); %stima stato
+    x(:,t+1) = A*x(:,t) + normrnd(0,1,[3,1]);                               %vero stato
+    y = C*x(:,t+1) + normrnd(0,1);                                          %vera uscita
+    [extX(:,t+1), covX] = ourKalman(A,B,C,x(:,t),u,y,covX,covErrX,covErrY); %stima stato
 end
 
-Qf = Q;
+subplot(2,1,1);     plot(T,x(:,:));     title("Vero stato");
+subplot(2,1,2);     plot(T,extX(:,:));  title("Stato stimato");
+
 [K_finito, K_infinito] = ourRiccatiSolver(A,B,Q,Qf,R,T);
