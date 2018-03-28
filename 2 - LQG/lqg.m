b6e899b240afb67e2535cfd4c6f661e1cc801a7c
Alessio Bollea <alessio.bollea@live.it>
2018-03-28 12:01:54 +0200

    Correzione simulazione lqg

--- 2 - LQG/lqg.m
+++ 2 - LQG/lqg.m
@@ -17,47 +17,50 @@ C = [1 1 -1];
 D = 0;
 
 u = zeros(5,1);
+extU = zeros(5,1);
 
 abs(eig(A))
 
 %Suppongo che lo stato iniziale sia distribuito come una gaussiana con
-%valor medio [10 5 -3] e covarianza [1 0 0; 0 1 0; 0 0 1]
-x0 = [10 5 -3]';
-x(:,1) = x0;      extX(:,1) = x0;
-covX = eye(3);
+%valor medio [10 5 -3] e covarianza nota
+mu = [10 5 -3]';
+x(:,1) = mu;
+sigma = 0.1*eye(3);
 
 %Suppongo che i rumori sullo stato e sull'uscita siano a media nulla e
-%varianza unitaria
-covErrX = eye(3);    covErrY = 1;
+%covarianza nota
+Q_noise = 0.1*eye(3);    R_noise = 0.1;
 
-%Simulazione per 8 istanti:
+%Parametri della simulazione
 sampleTime = 1;
 horizon = 7;
 T = 0:sampleTime:horizon;
-
-for t=1:length(T)-1
-    x(:,t+1) = A*x(:,t) + normrnd(0,1,[3,1]);                               %vero stato
-    y = C*x(:,t+1) + normrnd(0,1);                                          %vera uscita
-    [extX(:,t+1), covX] = ourKalman(A,B,C,x(:,t),u,y,covX,covErrX,covErrY); %stima stato
-end
-
-subplot(2,1,1);     plot(T,x(:,:));     title("Vero stato");
-subplot(2,1,2);     plot(T,extX(:,:));  title("Stato stimato");
 
 % Determinazione di K_finito e di K_infinito tramite nostra funzione di
 % Riccati
 [K_finito, K_infinito] = ourRiccatiSolver(A,B,Q,Qf,R,T);
 
-% Variabile per il grafico
-f_k_infinito = figure;
+%Simulazione del sistema reale
+stato_reale = figure;
+for t=1:sampleTime:horizon
+   u(:,t) = K_finito(:,:,t) * x(:,t);
+   x(:,t+1) = A*x(:,t) + B*u(:,t) + normrnd(0,0.1,[3,1]);
+   y(t) = C*x(:,t) + normrnd(0,0.1);
+end
+y(length(T)) = C*x(:,length(T)) + normrnd(0,0.1);          %serve per la simulazione successiva
+figure(stato_reale);
+subplot(3,1,1);     plot(T,x(1,:));     title('x1');
+subplot(3,1,2);     plot(T,x(2,:));     title('x2');
+subplot(3,1,3);     plot(T,x(3,:));     title('x3');
 
-% Utilizzo di K_infinito
-sistema = ss(A+B*K_infinito,B,C,D,sampleTime);
-U = zeros(5,length(T));
-[Y,Tsim,X] = lsim(sistema,U,T,x0);
-figure(f_k_infinito);
-
-% Grafici dell'andamento dello stato in LQG
-subplot(3,1,1); plot(Tsim,X(:,1));  title('x1');
-subplot(3,1,2); plot(Tsim,X(:,2));  title('x2');
-subplot(3,1,3); plot(Tsim,X(:,3));  title('x3');
+%Simulazione manuale usando K_finito
+extX(:,1) = mu;
+stato_stimato = figure;
+for t=1:sampleTime:horizon
+    extU(:,t) = K_finito(:,:,t) * extX(:,t);
+   [extX(:,t+1), sigma] = ourKalman(A,B,C,extX(:,t),extU(:,t),y(t+1),sigma,Q_noise,R_noise);
+end
+figure(stato_stimato);
+subplot(3,1,1);     plot(T,extX(1,:));     title('x1');
+subplot(3,1,2);     plot(T,extX(2,:));     title('x2');
+subplot(3,1,3);     plot(T,extX(3,:));     title('x3');
