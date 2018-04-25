51036d5235451f849ac87c490c14c2906dd63ea0
Alessio Bollea <alessio.bollea@live.it>
2018-04-25 17:29:02 +0200

    Correzione simulazione LQG
    Unite le due simulazioni. Modifiche ai nomi delle variabili della
    funzione di Kalman.

--- 2 - LQG/lqg.m
+++ 2 - LQG/lqg.m
@@ -16,16 +16,16 @@ B = [4 0 -1 0 0; 0 1 0 -1 0; 0 0 1 1 -1];
 C = [1 1 -1];
 D = 0;
 
-u = zeros(5,1);
-extU = zeros(5,1);
+estU = zeros(5,1);
+y = zeros(1,1);
 
 abs(eig(A))
 
 %Suppongo che lo stato iniziale sia distribuito come una gaussiana con
-%valor medio [10 5 -3] e covarianza nota
+%valor medio [10 5 7] e covarianza nota
 mu = [10 5 7]';
 x(:,1) = mu;
-var = 0.1;
+var = 0.5;
 sigma = var*eye(3);
 
 %Suppongo che i rumori sullo stato e sull'uscita siano a media nulla e
@@ -41,27 +41,38 @@ T = 0:sampleTime:horizon;
 % Riccati
 [K_finito, K_infinito] = ourRiccatiSolver(A,B,Q,Qf,R,T);
 
-%Simulazione del sistema reale
-stato_reale = figure;
-for t=1:sampleTime:horizon
-   u(:,t) = K_finito(:,:,t) * x(:,t);
-   x(:,t+1) = A*x(:,t) + B*u(:,t) + normrnd(0,var,[3,1]);
-   y(t) = C*x(:,t) + normrnd(0,var);
-end
-y(length(T)) = C*x(:,length(T)) + normrnd(0,var);          %serve per la simulazione successiva
-figure(stato_reale);
-subplot(3,1,1);     plot(T,x(1,:));     title('x1');
-subplot(3,1,2);     plot(T,x(2,:));     title('x2');
-subplot(3,1,3);     plot(T,x(3,:));     title('x3');
+%SIMULAZIONE DEL SISTEMA
+%Inizializzazione Kalman
+y(1) = C*x(:,1) + normrnd(0,var);
+predX = mu; predSigma = sigma;
+predY = C*predX;
+S = C*predSigma*C' + R_noise;
+H = predSigma*(C')*inv(S);
+estX(:,1) = predX + H*(y - predY);
+sigma = predSigma - H*S*H';
 
-%Simulazione manuale usando K_finito
-extX(:,1) = mu;
-stato_stimato = figure;
-for t=1:sampleTime:horizon
-    extU(:,t) = K_finito(:,:,t) * extX(:,t);
-   [extX(:,t+1), sigma] = ourKalman(A,B,C,extX(:,t),extU(:,t),y(t+1),sigma,Q_noise,R_noise);
+%Simulazione al primo istante
+estU(:,1) = K_finito(:,:,1) * estX(:,1);
+x(:,2) = A*x(:,1) + B*estU(:,1) + normrnd(0,var,[3,1]);
+
+%Simulazione agli istanti successivi
+for t=2:sampleTime:horizon
+   y(t) = C*x(:,t) + normrnd(0,var);
+   [estX(:,t),sigma] = ourKalman(A,B,C,estX(:,t-1),estU(:,t-1),y(t),sigma,Q_noise,R_noise);
+   estU(:,t) = K_finito(:,:,t) * estX(:,t);
+   x(:,t+1) = A*x(:,t) + B*estU(:,t) + normrnd(0,var,[3,1]);
 end
-figure(stato_stimato);
-subplot(3,1,1);     plot(T,extX(1,:));     title('x1');
-subplot(3,1,2);     plot(T,extX(2,:));     title('x2');
-subplot(3,1,3);     plot(T,extX(3,:));     title('x3');
+%Ultimo step (per i grafici)
+y(t+1) = C*x(:,t+1) + normrnd(0,var);
+[estX(:,t+1),sigma] = ourKalman(A,B,C,estX(:,t),estU(:,t),y(t+1),sigma,Q_noise,R_noise);
+
+%Grafici
+figure(1);
+subplot(3,1,1);     plot(T,x(1,:));      title('x1 vero');
+subplot(3,1,2);     plot(T,x(2,:));      title('x2 vero');
+subplot(3,1,3);     plot(T,x(3,:));      title('x3 vero');
+
+figure(2);
+subplot(3,1,1);     plot(T,estX(1,:));      title('x1 stimato');
+subplot(3,1,2);     plot(T,estX(2,:));      title('x2 stimato');
+subplot(3,1,3);     plot(T,estX(3,:));      title('x3 stimato');
--- 2 - LQG/ourKalman.m
+++ 2 - LQG/ourKalman.m
@@ -1,15 +1,15 @@
 %Questa funzione implementa il filtro di Kalman
 %Va chiamata ad ogni passo della simulazione
 %Input: stima dello stato precedente, controllo precedente, covarianze,
-%       uscita reale del sistema, modello del sistema
+%       uscita reale del sistema, modello del sistema (matrici A,B e C)
 %Covarianze: dello stato (sigma), rumori sullo stato (Q) e sull'uscita (R)
 %Output: stima dello stato e della covarianza dello stato ad un dato istante
-function [nextX,nextSigma] = ourKalman(A,B,C,prevX,prevU,y,prevSigma,Q,R)
-    extX = A*prevX + B*prevU;                  %predizione dello stato
-    extSigma = A*prevSigma*A' + Q;             %predizione covarianza stato
-    extY = C*extX;                             %predizione dell'uscita
-    S = C*extSigma*C' + R;                     %predizione covarianza uscita
-    H = extSigma*(C')*inv(S);                  %calcolo guadagno di Kalman
-    nextX = extX + H*(y - extY);               %stima dello stato
-    nextSigma = extSigma - H*S*H';             %stima covarianza stato
+function [estX,estSigma] = ourKalman(A,B,C,prevX,prevU,y,prevSigma,Q,R)
+    predX = A*prevX + B*prevU;                   %predizione dello stato
+    predSigma = A*prevSigma*A' + Q;              %predizione covarianza stato
+    predY = C*predX;                             %predizione dell'uscita
+    S = C*predSigma*C' + R;                      %predizione covarianza uscita
+    H = predSigma*(C')*inv(S);                   %calcolo guadagno di Kalman
+    estX = predX + H*(y - predY);                %stima dello stato
+    estSigma = predSigma - H*S*H';               %stima covarianza stato
 end
