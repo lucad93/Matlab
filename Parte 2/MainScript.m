794b42c1b7afd7dfe902f21dc548974f0f113e49
Alessio Bollea <alessio.bollea@live.it>
2018-05-18 16:59:38 +0200

    Prototipo fase backward

--- Parte 2/MainScript.m
+++ Parte 2/MainScript.m
@@ -28,7 +28,8 @@ for i=1:n+1                                         % per ongi stadio
        state{i}(j).jobs = comb{i}(j,:);
        state{i}(j).next = [];
        state{i}(j).prev = [];
-       state{i}(j).cost = [];  
+       state{i}(j).cost = [];                       % contiene tutti i costi per tutti gli stati successivi
+       state{i}(j).optimal = [];                    % contiene il costo ottimo e lo stato successivo ottimo
    end
 end
 
@@ -62,21 +63,45 @@ toc
 tic
 % Calcolo i costi per andare da uno stato all'altro, partendo dall'ultimo stadio
 % All'unico stato dell'ultimo stadio viene assegnato costo nullo
-state{n+1}.cost = 0;
+state{n+1}.optimal = [0 0];
 % Per tutti gli altri stadi
 for i=n:-1:1
     % Guardo tutti gli archi per passare dallo stadio attuale a quello successivo
     % Itero tra gli stati dello stadio attuale
     for j=1:length(state{i})
+       % Prima alloco il vettore dei costi di ogni stato come vettore di infiniti
+       state{i}(j).cost = Inf(1, length(state{i}(j).next));
        % Per ogni stato guardo tutti gli archi uscenti
        for k=1:length(state{i}(j).next)
           % Calcolo il costo tra lo stato attuale e il successivo (con l'arco che sto considerando)
           nextStateIndex = state{i}(j).next(k);
-          % Il costo è il costo per arrivare allo stato successivo
-          % + la somma dei processing time dei job che sono stati eseguiti nello stato attuale
-          % + la tardiness pesata del job che devo eseguire per andare allo stato successivo
-          
+          % Il costo è dato dalla somma dei processing time dei job eseguiti nello stato 
+          % in considerazione (st) + la tardiness (T) pesata (w), con
+          % T = massimo tra (st + processing time del job da eseguire per andare
+          % nello stato successivo - sua due time) e 0
+          % + il costo per andare nel nuovo stato
+          % Se però l'arco viola uno dei vincoli, viene lasciato costo infinito
+          if true %verifyConstraints(constraints)
+              % Calcolo st, sommando sui processing time dei job dello stato
+              st = 0;
+              stateJobs = state{i}(j).jobs;
+              for o=1:length(stateJobs)
+                  st = st + proc_time(o);
+              end
+              %Trovo il job che deve essere scelto
+              nextJob = setdiff(state{i+1}(nextStateIndex).jobs, state{i}(j).jobs);
+              % Calcolo T
+              T = max([(st+proc_time(nextJob)-due_time(nextJob)), 0]);
+              % Il costo totale è il costo dello stato successivo + la tardiness pesata
+              state{i}(j).cost(nextStateIndex) = weight(nextJob)*T + ...
+                  state{i+1}(nextStateIndex).optimal(1);
+          end
        end
+       % Quando ho calcolato tutti i costi per andare negli stati
+       % successivi, trovo quello minimo
+       [minCost, optStateIndex] = min(state{i}(j).cost);
+       % Assegno il minimo trovato a state.optimal nella forma [costoOttimo, statoOttimo]
+       state{i}(j).optimal = [minCost optStateIndex];
     end
 end
 toc
