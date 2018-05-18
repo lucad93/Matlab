4319930c8c21ec8184c484ccdc93d4454e8b2066
Alessio Bollea <alessio.bollea@live.it>
2018-05-18 16:06:08 +0200

    Programmazione dinamica
    Continuazione

--- Parte 2/MainScript.m
+++ Parte 2/MainScript.m
@@ -7,6 +7,9 @@ tic
 n = 3;                                              % numero di job
 jobs = 1:n;
 constraints = [2 1];                                % matrice dei vincoli
+proc_time = [4 2 6];                                % processing time di ogni job
+due_time = [7 9 9];                                 % due time di ogni job
+weight = [0.25 0.25 0.25];                          % peso di ogni job
 
 % Ogni stadio i (i va da 0 a 11) ha uno stato per ognuna delle combinazioni degli 11 job
 % presi a gruppi di i
@@ -31,7 +34,7 @@ end
 
 % Faccio i collegamenti tra gli stati di ogni stadio
 for i=1:n                               % esamino gli stadi contigui a coppie (i,i+1)
-   % esamino tutte le possibili coppie formabili tra gli stati di due stadi contigui
+   % Esamino tutte le possibili coppie formabili tra gli stati di due stadi contigui
    for j=1:length(state{i})
       for k=1:length(state{i+1})
           % se i==1 sto esaminando lo stadio 0, dove tutti i collegamenti sono possibili
@@ -57,9 +60,23 @@ toc
 
 %% Fase backward della programmazione dinamica
 tic
-% Considero due stadi contigui, partendo dall'ultimo
-for i=n+1:-1:2
-    
+% Calcolo i costi per andare da uno stato all'altro, partendo dall'ultimo stadio
+% All'unico stato dell'ultimo stadio viene assegnato costo nullo
+state{n+1}.cost = 0;
+% Per tutti gli altri stadi
+for i=n:-1:1
+    % Guardo tutti gli archi per passare dallo stadio attuale a quello successivo
+    % Itero tra gli stati dello stadio attuale
+    for j=1:length(state{i})
+       % Per ogni stato guardo tutti gli archi uscenti
+       for k=1:length(state{i}(j).next)
+          % Calcolo il costo tra lo stato attuale e il successivo (con l'arco che sto considerando)
+          nextStateIndex = state{i}(j).next(k);
+          % Il costo è il costo per arrivare allo stato successivo
+          % + la somma dei processing time dei job che sono stati eseguiti nello stato attuale
+          % + la tardiness pesata del job che devo eseguire per andare allo stato successivo
+          
+       end
+    end
 end
 toc
-
